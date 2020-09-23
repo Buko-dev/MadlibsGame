afd6f2b09d47108647d8ef0f2cfd1328c14b7755
DonGottijay <58939573+DonGottijay@users.noreply.github.com>
2020-09-23 23:12:05 +0100

    Add-MotorCycle Class

---
+++ MotoCycle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MadlibsGame
+{
+    class MotorCycle
+    {
+        public int driverIntensity;
+        public string name;
+
+        public MotorCycle(int intensity)
+        {
+            driverIntensity = intensity;
+        }
+
+        public void SetDriverName(string name)
+        {
+            this.name = name;
+        }
+    }
+}
